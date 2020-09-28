30ec8f4da3b517ead0226478d0d274b36e45f354
frank <frankjmclarke@github.com>
2020-09-27 17:18:46 -0700

    Initial commit

---
+++ app/src/main/java/com/fclarke/findphone/UserContact.kt
@@ -0,0 +1,10 @@
+package com.fclarke.findphone
+
+ class  UserContact{
+    var name:String?=null
+     var phoneNumber:String?=null
+     constructor(name:String,phoneNumber:String){
+         this.name=name
+         this.phoneNumber=phoneNumber
+     }
+ }
